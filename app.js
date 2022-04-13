76107f7838135579b32936f5e4a00e75e3875a58
Alisher Khayrullaev <39074542+khayrullaev@users.noreply.github.com>
2022-04-13 19:06:11 +0900

    [setup]...

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+var cookieParser = require("cookie-parser");
 var path = require("path");
+
+// Routes
 var apiRouter = require("./routes/api");
 var indexRouter = require("./routes/index");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 //Route Prefixes
