ad5c961bb5d0dcd883c9559ef6ac16c4ba8214c6
Alisher Khayrullaev <39074542+khayrullaev@users.noreply.github.com>
2022-04-27 18:06:13 +0900

    [routes] - product & user route added

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,12 @@ var app = express();
 // routers
 var authRouter = require("./auth");
 var testRouter = require("./test");
+var productRouter = require("./product");
+var userRouter = require("./user");
 
 app.use("/auth/", authRouter);
+app.use("/product/", productRouter);
+app.use("/user/", userRouter);
 app.use("/test/", testRouter);
 
 module.exports = app;
---
+++ routes/product.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+
+// controllers
+
+router.post("/list");
+
+module.exports = router;
