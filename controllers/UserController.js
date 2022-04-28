08e59b87d53bcb23277bbf5848701e32d7813b50
Alisher Khayrullaev <39074542+khayrullaev@users.noreply.github.com>
2022-04-28 18:37:03 +0900

    [controllers] - user controller

---
+++ controllers/UserController.js
@@ -0,0 +1,14 @@
+const info = async (req, res) => {};
+
+const editInfo = async (req, res) => {};
+
+const addToCart = async (req, res) => {};
+
+const removeFromCart = async (req, res) => {};
+
+module.exports = {
+  info,
+  editInfo,
+  addToCart,
+  removeFromCart,
+};
