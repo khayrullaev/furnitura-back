aac5a020320d3b7621893cae26149571d2ee2a8b
Alisher Khayrullaev <39074542+khayrullaev@users.noreply.github.com>
2022-06-16 18:10:02 +0900

    [routes] - home data

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,11 +1,32 @@
 var response = require("../utils/response");
+const Product = require("../models/Product");
 
 const getList = async (req, res, next) => {};
 
 const getOneProduct = async (req, res, next) => {};
 
-const getHomePageProducts = async (req, res, next) => {
-  return response.success(res, req.userId);
+const getHomePageProducts = async (req, res) => {
+  const collections = [
+    {
+      id: 1,
+      url: "https://res.cloudinary.com/dd4vsoahe/image/upload/v1653205673/furnitura/collections/pexels-max-vakhtbovych-6492397_mqxygh.jpg",
+    },
+    {
+      id: 2,
+      url: "https://res.cloudinary.com/dd4vsoahe/image/upload/v1653205643/furnitura/collections/pexels-pixabay-271816_yhu1n8.jpg",
+    },
+  ];
+  const popularProducts = await Product.find([{ $sample: { size: 2 } }]);
+  const saleProducts = await Product.aggregate([
+    { $match: { isSale: true } },
+    { $sample: { size: 2 } },
+  ]);
+
+  return response.successWithData(res, "Success", {
+    collections,
+    popularProducts,
+    saleProducts,
+  });
 };
 
 module.exports = {
