ca7b891e668c65c4a9e7d89b600e830647b12a8c
alisher <khayrullaev.ali@gmail.com>
2023-11-06 07:34:43 +0900

    [routes] - store

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -6,31 +6,52 @@ const getList = async (req, res, next) => {};
 const getOneProduct = async (req, res, next) => {};
 
 const getHomePageProducts = async (req, res) => {
-  const collections = [
-    {
-      id: 1,
-      url: "https://res.cloudinary.com/dd4vsoahe/image/upload/v1653205673/furnitura/collections/pexels-max-vakhtbovych-6492397_mqxygh.jpg",
-    },
-    {
-      id: 2,
-      url: "https://res.cloudinary.com/dd4vsoahe/image/upload/v1653205643/furnitura/collections/pexels-pixabay-271816_yhu1n8.jpg",
-    },
-  ];
-  const popularProducts = await Product.find([{ $sample: { size: 2 } }]);
-  const saleProducts = await Product.aggregate([
-    { $match: { isSale: true } },
-    { $sample: { size: 2 } },
-  ]);
+  try {
+    const collections = [
+      {
+        id: 1,
+        url: "https://res.cloudinary.com/dd4vsoahe/image/upload/v1653205673/furnitura/collections/pexels-max-vakhtbovych-6492397_mqxygh.jpg",
+      },
+      {
+        id: 2,
+        url: "https://res.cloudinary.com/dd4vsoahe/image/upload/v1653205643/furnitura/collections/pexels-pixabay-271816_yhu1n8.jpg",
+      },
+    ];
+    const popularProducts = await Product.aggregate([{ $sample: { size: 2 } }]);
+    const saleProducts = await Product.aggregate([
+      { $match: { isSale: true } },
+      { $sample: { size: 2 } },
+    ]);
 
-  return response.successWithData(res, "Success", {
-    collections,
-    popularProducts,
-    saleProducts,
-  });
+    return response.successWithData(res, "Success", {
+      collections,
+      popularProducts,
+      saleProducts,
+    });
+  } catch (error) {
+    return response.error(res, error.message);
+  }
+};
+
+const getStorePageProducts = async (req, res) => {
+  try {
+    const products = !!req.query.category
+      ? await Product.find({
+          category: req.query.category,
+        })
+      : await Product.aggregate().sample(8);
+
+    return response.successWithData(res, "Success", {
+      products,
+    });
+  } catch (error) {
+    return response.error(res, error.message);
+  }
 };
 
 module.exports = {
   getList,
   getOneProduct,
   getHomePageProducts,
+  getStorePageProducts,
 };
--- routes/product.js
+++ routes/product.js
@@ -6,9 +6,11 @@ const authenticate = require("../middlewares/authenticate");
 const {
   getList,
   getHomePageProducts,
+  getStorePageProducts,
 } = require("../controllers/ProductController");
 
 router.get("/list", authenticate, getList);
 router.get("/home", authenticate, getHomePageProducts);
+router.get("/store", authenticate, getStorePageProducts);
 
 module.exports = router;
